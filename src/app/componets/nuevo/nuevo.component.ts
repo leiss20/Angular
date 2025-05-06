0fef3264beb8849ebb8e8a5fbfd84cf5b3066f88
Leisson Murillo <leissonalvarez@gmail.com>
2025-05-06 18:31:47 -0500

    proyectoAngular

---
+++ src/app/componets/nuevo/nuevo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo',
+  imports: [],
+  templateUrl: './nuevo.component.html',
+  styleUrl: './nuevo.component.css'
+})
+export class NuevoComponent {
+
+}
